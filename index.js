cf8c0d49c35ff32b3e12c95850bc55a84cf30e38
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:03 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -560,7 +560,7 @@ instance.prototype.init_presets = function () {
 instance.prototype.actions = function() {
 	var self = this;
 
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'set_input': {
 			label: 'Set video input',
